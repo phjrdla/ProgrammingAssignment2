c08a1bc0e111818953ec3b7d536cad3290f69183
phjrdla <briensph@gmail.com>
2017-08-24 12:06:20 +0200

    second draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,86 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix : caches a matrix and its inverse
+## cacheSolve      : tries to use a previously cached inverse matrix data, if none is
+##                   found computes one
 
-## Write a short comment describing this function
-
+## makeCacheMatrix : function optional argument is a matrix.
+##                   Returns a list of functions to create and manipulate a 
+##                   matrix and its inverse. 
+## functions made available are
+## set : caches a matrix data. Invoking this function with an argument blanks 
+##       out inverse matrix data.
+## get : returnS cached matrix data
+## setminv : computes and caches inverse data of cached matrix
+## getminv ; returns cached inverse matrix data
+## variables
+## x : matrix data
+## m : inverse matrix data
 makeCacheMatrix <- function(x = matrix()) {
       m <- NULL
-      set <- function(y) {
+      
+      ## cache matrix data and
+      ## blank out any cached inverse matrix data
+      set <- function(y=NULL) {
+            if(is.null(y)) {
+                  message("provide a matrix")
+                  return (NULL)
+            }
             x <<- y
             m <<- NULL
       }
-      get <- function() x
-      setminv <- function(minv) m <<- minv
-      getminv <- function() m
+      
+      ## return cached matrix
+      get <- function() {
+            if (is.null(x)) {
+                  message("no matrix data found")
+                  return(NULL)
+            }
+            x
+      }
+      
+      ## compute inverse matrix and cache result
+      setminv <- function(){
+            if (is.null(x)) {
+                  message("no matrix data found")
+                  return(NULL)
+            }
+            message("inverse matrix is computed")
+            
+            m <<- solve(x)
+      }
+      
+      ## return cached inverse matrix data
+      getminv <- function() {
+            if (is.null(m)) {
+                  message("no inverse matrix data found")
+                  return(NULL)
+            }
+            m
+      }
+      
+      ## returns list of functions to manipulate matrix
       list(set = set, get = get, setminv = setminv, getminv = getminv)
 }
 
-## Write a short comment describing this function
-
+## cacheSolve : function argument is list returned by makeCacheMatrix
+##              returns inverse matrix data of matrix associated with argument
+##              computes inverse matrix when not found in cache
+## variables
+## x : list returned by makeCacheMatrix
+## m : cached inverse matrix data
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getminv()
+      
+      ## check for cached inverse matrix
+      if (!is.null(m)) {
+            message("getting cached inverse matrix")
+            return(m)
+      }
+      
+      ## compute and cache inverse matrix
+      x$setminv()
+      
+      ## return cache inverse matrix
+      x$getminv()
 }
